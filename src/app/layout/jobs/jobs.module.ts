83b3a2488d39f7b0f1cd1ea5a04042047c6f5a90
Stuart Woodman <stuart.woodman@csiro.au>
2018-07-19 15:09:06 +1000

    Log Preview now displays tabs for various log sections.

--- src/app/layout/jobs/jobs.module.ts
+++ src/app/layout/jobs/jobs.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ContextMenuModule } from 'primeng/contextmenu';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
+import { TabViewModule } from 'primeng/tabview';
 import { TableModule } from 'primeng/table';
 import { TreeTableModule } from 'primeng/treetable';
 
@@ -19,6 +20,7 @@ import { PlainTextPreview } from './preview/plaintext-preview.component';
 import { ImagePreview } from './preview/image-preview.component';
 import { PreviewDirective } from './preview/preview.directive';
 import { TtlPreview } from './preview/ttl-preview.component';
+import { LogPreview } from './preview/log-preview.component';
 import { OlMapModule } from '../datasets/openlayermap/olmap.module';
 import { JobBrowserComponent } from './job-browser.component';
 import { JobInputsComponent } from './job-inputs.component';
@@ -34,6 +36,7 @@ import { JobInputsBrowserModalContent } from './submission/job-inputs-browser.mo
         PageHeaderModule,
         ConfirmDialogModule,
         ContextMenuModule,
+        TabViewModule,
         TableModule,
         TreeTableModule,
         OlMapModule,
@@ -44,9 +47,9 @@ import { JobInputsBrowserModalContent } from './submission/job-inputs-browser.mo
         JobsComponent, JobBrowserComponent, JobInputsComponent,
         JobSubmissionDatasetsComponent, JobInputsBrowserModalContent,
         PreviewDirective, DataServicePreview, ImagePreview,
-        PlainTextPreview, TtlPreview, JobStatusModalContent
+        PlainTextPreview, TtlPreview, LogPreview, JobStatusModalContent
     ],
-    entryComponents: [ DataServicePreview, ImagePreview, PlainTextPreview, TtlPreview, JobStatusModalContent, JobInputsBrowserModalContent ],
+    entryComponents: [ DataServicePreview, ImagePreview, PlainTextPreview, TtlPreview, LogPreview, JobStatusModalContent, JobInputsBrowserModalContent ],
     providers: [ JobsService, ConfirmationService ],
     exports: [ JobSubmissionDatasetsComponent ]
 })
